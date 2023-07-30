339116677bd14bd28251ddbdf31ba58771a4471e
Yuriy Lunyov <lunyov.yu@gmail.com>
2023-07-30 22:38:40 +0300

    Added updateUser request, added NotFoundExceptions handling to the findUser request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,15 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -24,13 +27,24 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(Number(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(Number(id));
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
   }
 
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(Number(id), body);
   }
 
   @Delete('/:id')
