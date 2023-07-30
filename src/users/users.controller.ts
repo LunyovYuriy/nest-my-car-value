62f0b5d22e0ffae223855c338eff92be07322b99
Yuriy Lunyov <lunyov.yu@gmail.com>
2023-07-30 22:29:57 +0300

    added findUser, findAllUsers, removeUser requests to the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -11,8 +19,22 @@ export class UsersController {
     const user = await this.usersService.create(body.email, body.password);
 
     return {
-      message: 'success',
       id: user.id,
     };
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(Number(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(Number(id));
   }
 }
