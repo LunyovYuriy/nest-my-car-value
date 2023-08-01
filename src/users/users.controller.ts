2c9c4b2d3c6e46b8a02ecfab2b2531a22185d0d3
Yuriy Lunyov <lunyov.yu@gmail.com>
2023-08-01 21:50:55 +0300

    Added Serialize decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,13 +8,12 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { UsersService } from './users.service';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
@@ -29,7 +28,7 @@ export class UsersController {
     };
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(Number(id));
