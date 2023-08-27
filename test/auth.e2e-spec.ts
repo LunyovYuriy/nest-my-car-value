a8a702afceeefeccdd83182f7b65d1f7a939ec3e
Yuriy Lunyov <lunyov.yu@gmail.com>
2023-08-27 13:26:17 +0300

    added one more signup test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,22 @@ describe('Authentication System', () => {
         expect(email).toBe(email);
       });
   });
+
+  it('signup as a new user and get the currently logged in user', async () => {
+    const emailMock = 'test@mail.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: emailMock, password: 'password' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(emailMock);
+  });
 });
